c17b7fc5c6d110dfb07667410bb5a884ae22c072
Kai Schüler <kai.schueler@campus.tu-berlin.de>
2024-09-29 16:51:50 +0200

    gemini api update

--- gemini_api.py
+++ gemini_api.py
@@ -1,25 +1,70 @@
+import json
+from pprint import pprint
+
 import google.generativeai as genai
+import matplotlib.pyplot as plt
+import typing_extensions as typing
+
 from constants import GEMINI_API_KEY
 import PIL.Image
+from load_dataset_disc import get_dataset_split_generator
 
 GEMINI_MODEL = "gemini-1.5-pro-exp-0827"
-#GEMINI_MODEL = "gemini-1.5-flash-exp-0827"
+
+
+# GEMINI_MODEL = "gemini-1.5-flash-exp-0827"
+
+
+class GeminiModel:
+    def __init__(self, gemini_model_type=GEMINI_MODEL, temperature=None,
+                 templates_file="Data\\vqa_templates.yaml", prompt_file="Data\\vqa_gemini_prompt.txt"):
+        genai.configure(api_key=GEMINI_API_KEY)
+        self.model = genai.GenerativeModel(gemini_model_type)
+        self.config = genai.GenerationConfig(
+            response_mime_type="application/json", response_schema=list[QAPairGen], temperature=None)
+
+        with open(templates_file, "r") as f:
+            self.vqa_templates = f.read()
+
+        with open(prompt_file, "r") as f:
+            self.prompt = f.read()
+
+    def generate_qa(self, sample: dict):
+        img = sample['image']  # PILPng
+        label = sample['label']
+        caption = sample['caption']
+        inline_reference = sample['inline_reference']
+        metadata = sample['metadata']
+        acl_paper_id = sample['acl_paper_id']
+        pdf_text = sample['pdf_text']
+
+        metadata_string = f'Here is some additional info for the image:' \
+                          f'The label of the image is: {label}.' \
+                          f'The caption of the image is: {caption}.' \
+                          f'The inline reference of the image is: {inline_reference}.' \
+                          f'And the research paper as text where the image was sourced form is: {pdf_text[:10000]}'
+
+        return self.model.generate_content(
+            [self.prompt, self.vqa_templates, img, metadata_string])
+
+
+class QAPairGen(typing.TypedDict):
+    question_german: str
+    question_english: str
+    answer_english: str
+    answer_german: str
+
 
 if __name__ == '__main__':
-    genai.configure(api_key=GEMINI_API_KEY)
-    model = genai.GenerativeModel(GEMINI_MODEL)
-    generation_config = genai.GenerationConfig(temperature=2)
+    model = GeminiModel()
+    ds_iterator = iter(get_dataset_split_generator(split="train"))
+    ds_sample = next(ds_iterator)
 
-    f = open("Data\\vqa_templates.yaml", "r")
-    vqa_templates = f.read()
-    #print(vqa_templates)
-    sample_file1 = PIL.Image.open("D:\\MA\\ACLFig\\train\\image_0.jpg")
-    prompt = "Create a question about this figure. It should be a visual question, " \
-             "meaning mention at least on visual attribute like color, position or orientation of the different plot elements." \
-             " You should only be able to answer it if you look at the image, not just with extracted data as a table. But the question should not be to simple" \
-             "Also please provide the question in English and German. And then answer the question. Chose three random question templates as a base from the templates i provided to you. " \
-             "Not all templates fit all images. So choose on that fits and fill in the variables. Also alaways provide the template you used. Your questions do not have to match the template exactly. You can use it as a base"
-    response = model.generate_content([prompt, vqa_templates, sample_file1])
+    plt.imshow(ds_sample["image"])
+    plt.show()
+    response = model.generate_qa(ds_sample)
 
-    # response = model.generate_content("Write a story about a magic backpack.")
-    print(response.text)
+    json_response = json.loads(response.text)
+
+    print(response.usage_metadata)
+    pprint(json_response)
--- load_dataset_disc.py
+++ load_dataset_disc.py
@@ -1,11 +1,15 @@
 from datasets import load_from_disk
 import matplotlib.pyplot as plt
 
-dataset_path = 'Data\\VQAMeta'
+DATASET_PATH = 'Data\\VQAMeta'
 
-if __name__ == '__main__':
+
+def iterate_dataset_split(ds_split):
+    for s in ds_split:
+        yield s
+
+
+def get_dataset_split_generator(dataset_path=DATASET_PATH, split="train"):
     ds = load_from_disk(dataset_path)
-    for sample in ds["train"]:
-        img = sample["image"]
-        # plt.imshow(img)
-        # plt.show()
+    ds_split = ds[split]
+    return iterate_dataset_split(ds_split)
