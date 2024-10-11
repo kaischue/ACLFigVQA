63f5b61e2039d199f8c54f4eb5351004c097d057
Kai Schüler <kai.schueler@campus.tu-berlin.de>
2024-10-11 19:13:25 +0200

    self-refine feedback

--- gemini_api.py
+++ gemini_api.py
@@ -9,7 +9,7 @@ import pandas
 from constants import GEMINI_API_KEY
 from load_dataset_disc import get_dataset_split_generator
 from utils import load_yaml_to_dict, shuffle_lists_in_dict, find_first_mention_of_figure, remove_single_line_breaks, \
-    create_image_question_screenshot
+    create_image_question_screenshot, convert_csv_rows_json
 
 GEMINI_MODEL = "gemini-1.5-pro-latest"
 # GEMINI_MODEL = "gemini-1.5-flash-latest"
@@ -17,55 +17,12 @@ GEMINI_MODEL = "gemini-1.5-pro-latest"
 VISUAL_LABELS = ["bar charts", "boxplots", "confusion matrix", "Line graph_chart",
                  "pareto", "pie chart", "scatter plot", "venn diagram"]
 TEMPLATE_FILE = "Data\\vqa_templates_merged.yaml"
-QA_GEN_CSV_FILE = "Data\\VQAMeta\\test.csv"
+QA_GEN_CSV_FILE_REV1 = "Data\\qa_gen\\rev1\\qa_gen.csv"
+QA_GEN_CSV_FILE_REV2 = "Data\\qa_gen\\rev2\\qa_gen.csv"
 PROMPT_FILE = "Data\\vqa_gemini_prompt.txt"
-
-
-class GeminiModel:
-    def __init__(self, gemini_model_type=GEMINI_MODEL, temperature=None,
-                 templates_file=TEMPLATE_FILE, prompt_file=PROMPT_FILE):
-        genai.configure(api_key=GEMINI_API_KEY)
-        self.model = genai.GenerativeModel(gemini_model_type)
-        self.config = genai.GenerationConfig(
-            response_mime_type="application/json", response_schema=list[QAPairGen], temperature=None)
-
-        self.vqa_templates = load_yaml_to_dict(templates_file)
-
-        with open(prompt_file, "r") as f:
-            self.prompt = f.read()
-
-    def generate_qa(self, sample: dict, shuffle=False, figure_mention_range=5000):
-        img = sample['image']  # PILPng
-        label = sample['label']
-        caption = sample['caption']
-        inline_reference = sample['inline_reference']
-        metadata = sample['metadata']
-        acl_paper_id = sample['acl_paper_id']
-        pdf_text = remove_single_line_breaks(sample['pdf_text'])
-
-        first_mention_idx = find_first_mention_of_figure(pdf_text, caption)
-
-        metadata_string = f'Here is some additional info for the image:' \
-                          f'The label of the image is: {label}.' \
-                          f'The caption of the image is: {caption}.' \
-                          f'The inline reference of the image is: {inline_reference}.' \
-                          f'And the relevant section from the research paper that includes the figure caption. ' \
-                          f'This section starts from {figure_mention_range} characters before and after the first ' \
-                          f'mention of the figure caption:\n' \
-                          f'{pdf_text[max([0, first_mention_idx - figure_mention_range]):min([first_mention_idx + figure_mention_range, len(pdf_text)])]}'
-
-        if shuffle:
-            shuffle_lists_in_dict(self.vqa_templates)
-
-        template_str_dict = str(self.vqa_templates)
-
-        return self.model.generate_content(
-            [self.prompt, template_str_dict, img, metadata_string])
-
-    def try_fix_json(self, broken_json, error_msg):
-        return self.model.generate_content(
-            [f"Can you please fix this broken JSON? I get this error msg:{error_msg}. Only reply with the fixed JSON "
-             f"string, nothing else! So that i can directly parse it!", broken_json])
+FEEDBACK_FILE = "Data\\feedback_prompt.txt"
+IMPROVEMENT_FILE = "Data\\improvement_prompt.txt"
+JSON_FIX_PROMPT = "Data\\json_fix_prompt.txt"
 
 
 @dataclass
@@ -76,57 +33,195 @@ class QAPairGen:
     answer_german: str
 
 
+class GeminiModel:
+    def __init__(self, gemini_model_type=GEMINI_MODEL, temperature=None, figure_mention_range=5000,
+                 templates_file=TEMPLATE_FILE, prompt_file=PROMPT_FILE, feedback_file=FEEDBACK_FILE,
+                 improvement_file=IMPROVEMENT_FILE):
+        genai.configure(api_key=GEMINI_API_KEY)
+        self.model = genai.GenerativeModel(gemini_model_type)
+        self.config = genai.GenerationConfig(
+            response_mime_type="application/json", response_schema=list[QAPairGen], temperature=None)
+
+        self.vqa_templates = load_yaml_to_dict(templates_file)
+
+        with open(prompt_file, "r") as f:
+            self.prompt = f.read()
+
+        with open(feedback_file, "r") as f:
+            self.feedback_prompt = f.read()
+
+        with open(improvement_file, "r") as f:
+            self.improvement_prompt = f.read()
+
+        with open(JSON_FIX_PROMPT, "r") as f:
+            self.json_fix_prompt = f.read()
+
+        self.figure_mention_range = figure_mention_range
+
+    def get_metadata_str(self, sample: dict):
+        img = sample['image']  # PILPng
+        label = sample['label']
+        caption = sample['caption']
+        inline_reference = sample['inline_reference']
+        metadata = sample['metadata']
+        acl_paper_id = sample['acl_paper_id']
+        pdf_text = remove_single_line_breaks(sample['pdf_text'])
+
+        first_mention_idx = find_first_mention_of_figure(pdf_text, caption)
+
+        relevant_pdf_str = pdf_text[
+                           max([0, first_mention_idx - self.figure_mention_range]
+                               ):min([first_mention_idx + self.figure_mention_range, len(pdf_text)])
+                           ]
+
+        metadata_string = f'Here is some additional info for the image:' \
+                          f'The label of the image is: {label}.' \
+                          f'The caption of the image is: {caption}.' \
+                          f'The inline reference of the image is: {inline_reference}.' \
+                          f'And the relevant section from the research paper that includes the figure caption. ' \
+                          f'This section starts from {self.figure_mention_range} characters before and after the first ' \
+                          f'mention of the figure caption:\n' \
+                          f'{relevant_pdf_str}'
+        return metadata_string
+
+    def generate_qa(self, sample: dict, shuffle=False):
+        img = sample['image']  # PILPng
+        metadata_string = self.get_metadata_str(sample)
+
+        if shuffle:
+            shuffle_lists_in_dict(self.vqa_templates)
+
+        template_str_dict = str(self.vqa_templates)
+
+        return self.model.generate_content(
+            [self.prompt, template_str_dict, img, metadata_string])
+
+    def generate_feedback(self, sample: dict, qa_pairs: str):
+        img = sample['image']  # PILPng
+        metadata_string = self.get_metadata_str(sample)
+
+        template_str_dict = str(self.vqa_templates)
+
+        return self.model.generate_content(
+            [self.feedback_prompt, qa_pairs, template_str_dict, img, metadata_string])
+
+    def generate_qa_revised(self, sample: dict, qa_pairs: str, feedback: str):
+        img = sample['image']  # PILPng
+        metadata_string = self.get_metadata_str(sample)
+
+        template_str_dict = str(self.vqa_templates)
+
+        return self.model.generate_content(
+            [self.improvement_prompt, feedback, qa_pairs, template_str_dict, img, metadata_string])
+
+    def try_fix_json(self, broken_json, error_msg):
+        return self.model.generate_content(
+            [self.json_fix_prompt, broken_json, error_msg])
+
+
 def write_row_to_csv(csv_path, row, mode='a'):
     with open(csv_path, mode=mode, newline='', encoding="utf-8") as f:
         writer = csv.writer(f)
         writer.writerow(row)
 
 
+def fix_and_save_qa(gemini_response, gemini_model: GeminiModel, qa_gen_csv_file):
+    try:
+        json_response = json.loads(gemini_response.text)
+    except json.JSONDecodeError as e:
+        print("Invalid JSON, prompting for fix...")
+        e_msg = str(e)
+        invalid_json = e.doc
+        fix_response = gemini_model.try_fix_json(invalid_json, e_msg)
+        json_response = json.loads(fix_response.text)
+
+    print(gemini_response.usage_metadata)
+
+    try:
+        qa_objects = []
+        for qa in json_response:
+            qa_object = QAPairGen(**qa)
+            qa_objects.append(qa_object)
+    except Exception as e:
+        fix_response = gemini_model.try_fix_json(json_response, str(e))
+        json_response = json.loads(fix_response.text)
+    finally:
+        qa_objects = []
+        for qa in json_response:
+            qa_object = QAPairGen(**qa)
+            qa_objects.append(qa_object)
+
+    for qa_object in qa_objects:
+        write_row_to_csv(qa_gen_csv_file, [
+            ds_sample["img_file_name"],
+            qa_object.question_german,
+            qa_object.question_english,
+            qa_object.answer_german,
+            qa_object.answer_english])
+
+
+def get_rows_and_save_img_qa(sample, qa_gen_csv_file, out_path):
+    df_qa_gen_train = pandas.read_csv(qa_gen_csv_file)
+
+    rows = df_qa_gen_train.loc[df_qa_gen_train['img_file_name'] == sample["img_file_name"]]
+    questions_eng = rows.question_english.values
+    answers_eng = rows.answer_english.values
+    questions_de = rows.question_german.values
+    answers_de = rows.answer_german.values
+    questions = questions_eng + ' | ' + questions_de
+    answers = answers_eng + ' | ' + answers_de
+
+    create_image_question_screenshot(
+        f"Data\\VQAMeta\\training_data\\train\\{sample['label']}\\{sample['img_file_name']}",
+        questions, answers, out_path=out_path)
+    return convert_csv_rows_json(rows)
+
+
 if __name__ == '__main__':
     model = GeminiModel()
     ds_iterator = iter(get_dataset_split_generator(split="train"))
 
-    if not os.path.isfile(QA_GEN_CSV_FILE):
-        write_row_to_csv(QA_GEN_CSV_FILE,
+    if not os.path.isfile(QA_GEN_CSV_FILE_REV1):
+        write_row_to_csv(QA_GEN_CSV_FILE_REV1,
+                         ["img_file_name", "question_german", "question_english", "answer_german", "answer_english"],
+                         mode='w')
+    if not os.path.isfile(QA_GEN_CSV_FILE_REV2):
+        write_row_to_csv(QA_GEN_CSV_FILE_REV2,
                          ["img_file_name", "question_german", "question_english", "answer_german", "answer_english"],
                          mode='w')
 
-    df_qa_gen_train = pandas.read_csv(QA_GEN_CSV_FILE)
+    df_qa_gen_train_rev1 = pandas.read_csv(QA_GEN_CSV_FILE_REV1)
 
     try:
         while True:
-
             ds_sample = next(ds_iterator)
             if ds_sample["label"] not in VISUAL_LABELS:
                 continue
-            if not df_qa_gen_train.loc[df_qa_gen_train['img_file_name'] == ds_sample["img_file_name"]].empty:
-                rows = df_qa_gen_train.loc[df_qa_gen_train['img_file_name'] == ds_sample["img_file_name"]]
-                questions = rows.question_english
-                answers = rows.answer_english
-                create_image_question_screenshot(f"Data\\VQAMeta\\training_data\\train\\{ds_sample['label']}\\{ds_sample['img_file_name']}", questions.values, answers.values)
-                continue
-            # plt.imshow(ds_sample["image"])
-            # plt.show()
-            response = model.generate_qa(ds_sample, shuffle=True)
 
-            try:
-                json_response = json.loads(response.text)
-            except json.JSONDecodeError as e:
-                print("Invalid JSON, prompting for fix...")
-                e_msg = str(e)
-                invalid_json = e.doc
-                fix_response = model.try_fix_json(invalid_json, e_msg)
-                json_response = json.loads(fix_response.text)
+            # generate qa
+            if df_qa_gen_train_rev1.loc[df_qa_gen_train_rev1['img_file_name'] == ds_sample["img_file_name"]].empty:
+                response = model.generate_qa(ds_sample, shuffle=True)
+                fix_and_save_qa(response, model, QA_GEN_CSV_FILE_REV1)
 
-            print(response.usage_metadata)
-            for qa in json_response:
-                qa_object = QAPairGen(**qa)
-                write_row_to_csv(QA_GEN_CSV_FILE, [
-                    ds_sample["img_file_name"],
-                    qa_object.question_german,
-                    qa_object.question_english,
-                    qa_object.answer_german,
-                    qa_object.answer_english])
-            # pprint(json_response)
+            qa_pairs_str = get_rows_and_save_img_qa(ds_sample, QA_GEN_CSV_FILE_REV1, out_path="Data\\qa_gen\\rev1")
+
+            # generate feedback
+            feedback_file_f = f"Data\\qa_gen\\rev1\\{ds_sample['img_file_name']}_feedback.txt"
+            if not os.path.isfile(feedback_file_f):
+                s_feedback = model.generate_feedback(ds_sample, qa_pairs_str)
+                with open(feedback_file_f, 'w') as file:
+                    file.write(s_feedback.text)
+
+            # generate qa revised
+            df_qa_gen_train_rev2 = pandas.read_csv(QA_GEN_CSV_FILE_REV2)
+            if df_qa_gen_train_rev2.loc[df_qa_gen_train_rev2['img_file_name'] == ds_sample["img_file_name"]].empty:
+                with open(feedback_file_f, 'r') as file:
+                    s_feedback = file.read()
+                response_improved = model.generate_qa_revised(ds_sample, qa_pairs_str, s_feedback)
+                fix_and_save_qa(response_improved, model, QA_GEN_CSV_FILE_REV2)
+
+            qa_pairs_str_rev2 = get_rows_and_save_img_qa(ds_sample, QA_GEN_CSV_FILE_REV2, out_path="Data\\qa_gen\\rev2")
+            print("Done with img!")
+
     except StopIteration:
         pass
--- utils.py
+++ utils.py
@@ -19,7 +19,9 @@ def shuffle_lists_in_dict(yaml_dict):
             shuffle(yaml_dict[key][k_key])
 
 
-def create_image_question_screenshot(img_path, questions, answers):
+def create_image_question_screenshot(img_path, questions, answers, out_path):
+
+    out_name = f"{out_path}\\{Path(img_path).stem}_qa.png"
     # Open your chart image
     chart = Image.open(img_path)
 
@@ -62,7 +64,7 @@ def create_image_question_screenshot(img_path, questions, answers):
         y_text += 20  # Extra space between questions
 
     # Save the new image
-    new_img.save(f"Data\\sample_qa\\{Path(img_path).stem}_qa.png")
+    new_img.save(out_name)
 
 
 def remove_single_line_breaks(text):
@@ -88,6 +90,25 @@ def find_first_mention_of_figure(text, caption):
     pdf_text_no_lb = text.lower()
     figure_i_index = find_first_number_end_index(caption)
     return pdf_text_no_lb.find(caption[:figure_i_index].lower())
+
+
+def convert_csv_rows_json(rows):
+    questions_eng = rows.question_english.values
+    answers_eng = rows.answer_english.values
+    questions_de = rows.question_german.values
+    answers_de = rows.answer_german.values
+    qa_pairs = []
+
+    for q_de, q_eng, a_de, a_eng in zip(questions_de, questions_eng, answers_de, answers_eng):
+        qa_pair = {
+            "question_german": q_de,
+            "question_english": q_eng,
+            "answer_english": a_eng,
+            "answer_german": a_de
+        }
+        qa_pairs.append(qa_pair)
+
+    return str(qa_pairs)
 
 
 if __name__ == '__main__':
